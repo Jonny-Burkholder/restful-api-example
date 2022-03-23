688c6c529fb4779c9e435765904bf3cace4c4388
Jonny-Burkholder <48441498+Jonny-Burkholder@users.noreply.github.com>
2022-03-22 22:54:46 -0400

    update back-end

--- internal/item/item.go
+++ internal/item/item.go
@@ -16,7 +16,7 @@ var errNotCheckedOut = errors.New("Item has not been checked out")
 //in an API
 type Item interface {
 	CheckOut(*user.User) error
-	ReturnItem() error
+	ReturnItem(*user.User) error
 }
 
 type dvd struct {
@@ -31,26 +31,53 @@ type dvd struct {
 //checkOut changes the checkedout bool to true. It returns a non-nil
 //error if the item is already checked out
 func (d *dvd) CheckOut(user *user.User) error {
-	if d.CheckedOut == true {
+	//first, let's check to see if the item is already checked out
+	if d.CheckedOut {
+		//if so, return an error
 		return errAlreadyOut
 	}
+	//if not, add it to the user's checked out items
+	user.ItemsOut.Store(d, true)
+	//mark it as checked out
 	d.CheckedOut = true
+	//no error, return nil
 	return nil
 }
 
 //return changes the checked-out bool to false. It returns a non-nil
 //error if the item is not currently checked out
-func (d *dvd) ReturnItem() error {
-	if d.CheckedOut == false {
+func (d *dvd) ReturnItem(user *user.User) error {
+	if !d.CheckedOut {
 		return errNotCheckedOut
 	}
+	user.ItemsOut.Delete(d)
 	d.CheckedOut = false
 	return nil
 }
 
+//tapes are boring, we'll keep them to a minimum
 type tape struct {
-	Title   string `json:"title"`
-	Runtime string `json:"runtime"`
+	Title      string `json:"title"`
+	Runtime    string `json:"runtime"`
+	CheckedOut bool   `json:"checked_out"`
+}
+
+func (t *tape) CheckOut(user *user.User) error {
+	if t.CheckedOut {
+		return errAlreadyOut
+	}
+	user.ItemsOut.Store(t, true)
+	t.CheckedOut = true
+	return nil
+}
+
+func (t *tape) ReturnItem(user *user.User) error {
+	if !t.CheckedOut {
+		return errNotCheckedOut
+	}
+	user.ItemsOut.Delete(t)
+	t.CheckedOut = false
+	return nil
 }
 
 type book struct {
@@ -58,4 +85,23 @@ type book struct {
 	Author         string `json:"author"`
 	Genre          string `json:"genre"`
 	PublishingDate string `json:"publishing_date"`
+	CheckedOut     bool   `json:"checked_out"`
+}
+
+func (b *book) CheckOut(user *user.User) error {
+	if b.CheckedOut {
+		return errAlreadyOut
+	}
+	user.ItemsOut.Store(b, true)
+	b.CheckedOut = true
+	return nil
+}
+
+func (b *book) ReturnItem(user *user.User) error {
+	if !b.CheckedOut {
+		return errAlreadyOut
+	}
+	user.ItemsOut.Delete(b)
+	b.CheckedOut = false
+	return nil
 }
--- internal/user/user.go
+++ internal/user/user.go
@@ -1,9 +1,30 @@
 package user
 
+import (
+	"fmt"
+	"sync"
+)
+
 //We'll keep this nice and simple
 type User struct {
-	UserID    string `json:"user_id"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Email     string `json:"email"`
+	FirstName string   `json:"first_name"`
+	LastName  string   `json:"last_name"`
+	Email     string   `json:"email"`
+	ItemsOut  sync.Map `json:"items_out"`
+}
+
+//NewUser takes a first name, last name, and email,
+//and returns a pointer to a user
+func NewUser(first, last, email string) *User {
+	return &User{
+		FirstName: first,
+		LastName:  last,
+		Email:     email,
+		ItemsOut:  sync.Map{},
+	}
+}
+
+//EmailUser emails a user to remind them that they need to return a book
+func (u *User) EmailUser() {
+	fmt.Println("Return your item!")
 }
