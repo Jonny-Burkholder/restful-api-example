536ae8122f3f4043717cd00d74a5098c597ff38b
Jonny-Burkholder <48441498+Jonny-Burkholder@users.noreply.github.com>
2022-03-23 19:22:19 -0400

    Update item_handler.go

--- internal/item/item_handler.go
+++ internal/item/item_handler.go
@@ -1,10 +1,21 @@
 package item
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func handleItem(ds interface{}) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
-
+		if r.Method == http.MethodGet {
+			//get the item
+			q := r.URL.Query()
+			fmt.Println(q)
+		} else if r.Method == http.MethodPost {
+			//return the item
+		} else {
+			http.Error(w, "Invalid request", http.StatusBadRequest)
+		}
 	}
 	return http.HandlerFunc(fn)
 }
